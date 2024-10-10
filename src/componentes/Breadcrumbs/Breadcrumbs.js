bff7e38d9d0e48a1c57107d6314d86e96f67cbfa
Aprendiz <Aprendiz@BOGDFPCGMF3219.FormacionSena.RED>
2024-10-10 09:51:42 -0500

    Se avanzó los breadcrumbs

---
+++ src/componentes/Breadcrumbs/Breadcrumbs.js
@@ -0,0 +1,27 @@
+// Breadcrumbs.js
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import './Breadcrumbs.css'; // Crea un archivo CSS para estilos
+
+const Breadcrumbs = () => {
+    const location = useLocation();
+    const pathnames = location.pathname.split('/').filter(x => x);
+
+    return (
+        <nav aria-label="breadcrumb" className="breadcrumb">
+            <ol>
+                <li><Link to="/">Inicio</Link></li>
+                {pathnames.map((pathname, index) => {
+                    const routeTo = `/${pathnames.slice(0, index + 1).join('/')}`;
+                    return (
+                        <li key={routeTo}>
+                            <Link to={routeTo}>{pathname.charAt(0).toUpperCase() + pathname.slice(1)}</Link>
+                        </li>
+                    );
+                })}
+            </ol>
+        </nav>
+    );
+}
+
+export default Breadcrumbs;
--- src/componentes/Header/Header.js
+++ src/componentes/Header/Header.js
@@ -6,11 +6,12 @@ import cart from '../../assets/icons/cart.png';
 import account from '../../assets/icons/account.png';
 import CartModal from '../CartModal/CartModal';
 import AccountModal from '../AccountModal/AccountModal';
+import Breadcrumbs from '../Breadcrumbs/Breadcrumbs'; // Asegúrate de que la ruta sea correcta
 
 const Header = () => {
     const [isCartModalOpen, setIsCartModalOpen] = useState(false);
     const [isAccountModalOpen, setIsAccountModalOpen] = useState(false);
-    const [isAnimating, setIsAnimating] = useState(false);
+    const [, setIsAnimating] = useState(false);
     const [isHeaderExpanded, setIsHeaderExpanded] = useState(false);
     const location = useLocation();
     const path = location.pathname;
@@ -31,7 +32,7 @@ const Header = () => {
                 }, 100);
             }, 150);
         } else {
-            closeModals(); // Cierra la otra modal si está abierta
+            closeModals(); 
             setIsHeaderExpanded(true);
             setTimeout(() => {
                 setIsCartModalOpen(true);
@@ -51,7 +52,7 @@ const Header = () => {
                 }, 100);
             }, 150);
         } else {
-            closeModals(); // Cierra la otra modal si está abierta
+            closeModals(); 
             setIsHeaderExpanded(true);
             setTimeout(() => {
                 setIsAccountModalOpen(true);
@@ -61,35 +62,40 @@ const Header = () => {
     };
 
     return (
-        <header className="header">
-            <div className="header__container">
-                <div className='logo-header__container'>
-                    <Link to={"/"}><img src={logo} alt="Logo" className="header__logo" /></Link>
-                </div>
-
-                <div className='navigation-header__container'>
-                    <div className={`sections-header__container ${isHeaderExpanded ? 'expanded' : ''}`}>
-                        <Link to={"/Nosotros"} className={path === "/Nosotros" ? "active" : ""}>NOSOTROS</Link>
-                        <Link to={"/Productos"} className={path === "/Productos" ? "active" : ""}>PRODUCTOS</Link>
-                        <Link to={"/Contacto"} className={path === "/Contacto" ? "active" : ""}>CONTÁCTENOS</Link>
+        <>
+            <header className="header">
+                <div className="header__container">
+                    <div className='logo-header__container'>
+                        <Link to={"/"}><img src={logo} alt="Logo" className="header__logo" /></Link>
                     </div>
 
-                    <div className={`icons-header__container ${isHeaderExpanded ? 'expanded' : ''}`}>
-                        <ul className="header__list">
-                            <li className="header__element">
-                                <img src={cart} alt="Carrito" className="cart" onClick={toggleCartModal} />
-                            </li>
-                            <li className="header__element">
-                                <img src={account} alt='Cuenta' className='account' onClick={toggleAccountModal} />
-                            </li>
-                        </ul>
+                    <div className='navigation-header__container'>
+                        <div className={`sections-header__container ${isHeaderExpanded ? 'expanded' : ''}`}>
+                            <Link to={"/Nosotros"} className={path === "/Nosotros" ? "active" : ""}>NOSOTROS</Link>
+                            <Link to={"/Productos"} className={path === "/Productos" ? "active" : ""}>PRODUCTOS</Link>
+                            <Link to={"/Contacto"} className={path === "/Contacto" ? "active" : ""}>CONTÁCTENOS</Link>
+                        </div>
+
+                        <div className={`icons-header__container ${isHeaderExpanded ? 'expanded' : ''}`}>
+                            <ul className="header__list">
+                                <li className="header__element">
+                                    <img src={cart} alt="Carrito" className="cart" onClick={toggleCartModal} />
+                                </li>
+                                <li className="header__element">
+                                    <img src={account} alt='Cuenta' className='account' onClick={toggleAccountModal} />
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </header>
+
+            {/* Integrar el componente de Breadcrumbs */}
+            <Breadcrumbs />
 
             <CartModal isOpen={isCartModalOpen} onClose={toggleCartModal} />
             <AccountModal isOpen={isAccountModalOpen} onClose={toggleAccountModal} />
-        </header>
+        </>
     );
 }
 
