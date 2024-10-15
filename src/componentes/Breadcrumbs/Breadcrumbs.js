12afd19354b902baf6beb477e26f815d62cbd244
Davidddd222 <davidvergaranacun9@gmail.com>
2024-10-14 21:37:22 -0500

    Se actualizaron los estilos de los breadcrumbs
    Cambia el símbolo de separación en los breadcrumbs de números"1-2-3..." a "›".

--- src/componentes/Breadcrumbs/Breadcrumbs.js
+++ src/componentes/Breadcrumbs/Breadcrumbs.js
@@ -1,7 +1,7 @@
 // Breadcrumbs.js
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import './Breadcrumbs.css'; // Crea un archivo CSS para estilos
+import './Breadcrumbs.css';
 
 const Breadcrumbs = () => {
     const location = useLocation();
@@ -12,7 +12,7 @@ const Breadcrumbs = () => {
             <ol>
                 <li><Link to="/">Inicio</Link></li>
                 {pathnames.map((pathname, index) => {
-                    const routeTo = `/${pathnames.slice(0, index + 1).join('/')}`;
+                    const routeTo = `/${pathnames.slice(0, index + 1).join('/')}`; // Corrección aquí
                     return (
                         <li key={routeTo}>
                             <Link to={routeTo}>{pathname.charAt(0).toUpperCase() + pathname.slice(1)}</Link>
